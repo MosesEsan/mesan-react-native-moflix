951e955f7a0b8a0779fbae1ef10a564a71566a58
Moses Esan <mosesesan@hotmail.com>
2024-10-20 02:02:28 +0100

    Added Main Module Core Files

---
+++ app/modules/main/core/Config.js
@@ -0,0 +1,12 @@
+// 1 - COLORS
+// THIS CAN BE HERE OR MOVED TO A SEPARATE FILE ESPECIALLY IF IT AFFECCTS MULTIPLE MODULES
+export const colors = {
+    primary: "#141414",
+    secondary: "rgb(45, 45, 45)",
+    text: "#e5e5e5"
+}
+
+// 2 - IMAGE AND VIDEO URLS
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
+export const LARGE_IMAGE_URL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
+export const YOUTUBE_URL = "https://img.youtube.com/vi/";
