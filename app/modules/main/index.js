1b6661ee95ef855e45356e1a4d44fa141b421449
Moses Esan <mosesesan@hotmail.com>
2024-10-20 02:43:17 +0100

    Main Module Index file

---
+++ app/modules/main/index.js
@@ -0,0 +1,7 @@
+export {default as Provider} from './core/Provider';
+// export {default as Reducer} from './core/Reducer';
+import * as Service from './core/Service';
+export {default as Route} from './core/Route';
+import * as Config from './core/Config';
+
+export {Config, Service};
