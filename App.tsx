cdd5e9b48e8935f5ea7a53c984890f9f2969233a
Moses Esan <mosesesan@hotmail.com>
2024-10-22 02:20:32 +0100

    Added the react-query package

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import RootNavigator from "./app/navigation/AppRoute";
 //import RootNavigator from "./app/navigation/AppTabs";
 import ProviderContainer from "./app/ProviderContainer";
 
-export default function App() : React.JSX.Element {
+// Create a client
+const queryClient = new QueryClient()
+
+export default function App(): React.JSX.Element {
   return (
     <ProviderContainer>
-      <RootNavigator/>
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+      </QueryClientProvider>
     </ProviderContainer>
   );
 }
