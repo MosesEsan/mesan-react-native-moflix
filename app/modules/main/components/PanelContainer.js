b95d475ff803a1b54415b5f0ad04b321f477f9e2
Moses Esan <mosesesan@hotmail.com>
2024-10-29 04:32:15 +0000

    Updated the PanelContainer Component

--- app/modules/main/components/PanelContainer.js
+++ app/modules/main/components/PanelContainer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Panel from "mesan-react-native-panel";
 
 const PanelContainer = ({ data = [] }) => {
@@ -5,8 +6,10 @@ const PanelContainer = ({ data = [] }) => {
         <>
             {
                 data.map((panel, index) => {
+                    let renderItem = (panel?.renderItem) ? ({ item, index }) => panel?.renderItem({ item, index, panel }): null
+                    let onPress = (panel?.onPress) ? () => panel.onPress(panel) : null;
                     return (
-                        <Panel key={`panel_${index}`} {...panel}/>
+                        <Panel key={`panel_${index}`} {...panel} renderItem={renderItem} onPress={onPress}/>
                     )
                 })
             }
