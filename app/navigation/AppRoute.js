d87e6b4198e759c68f386876c5441d147b98f30f
Moses Esan <mosesesan@hotmail.com>
2024-11-25 01:07:25 +0000

    Restructure the App Route

--- app/navigation/AppRoute.js
+++ app/navigation/AppRoute.js
@@ -6,12 +6,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import BootSplash from "react-native-bootsplash";
 
-// import AppTabs from './AppTabs';
+import AppTabs from './AppTabs';
 
 // If not using Tabs - Import the Module Routes
-import ModuleRoute from "../modules/main/core/Route";
+// import ModuleRoute from "../modules/main/core/Route";
 
-const Stack = createStackNavigator();
+// if using tabs and a view is shared by multiple sections, import it here
+import {
+  ListScreen,
+  DetailsScreen,
+  CreditsScreen,
+  MediaPlayer
+} from "../modules/main/core/Route";
+
+// CONFIG
+import { colors } from '../modules/main/core/Config';
 
 export default function AppRoute() {
   //2 - MAIN CODE BEGINS HERE
@@ -21,12 +30,32 @@ export default function AppRoute() {
     }, 3000)
   },);
 
+  const screenOptions = {
+    headerStyle: {
+      borderBottomWidth: 0,
+      shadowColor: 'transparent',
+      backgroundColor: colors.primary,
+    },
+    headerTitleStyle: { color: colors.text },
+    headerTintColor: '#fff'
+  };
+
+  // 3 - CREATE THE NAVIGATOR
+  const RootStack = createStackNavigator();
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="App" component={ModuleRoute} options={{ headerShown: false }} />
-        </Stack.Navigator>
+        <RootStack.Navigator>
+          <RootStack.Group>
+            <RootStack.Screen name="App" component={AppTabs} options={{ headerShown: false }} />
+            <RootStack.Screen name={"List"} component={ListScreen} options={{ ...screenOptions }} />
+            <RootStack.Screen name={"Credits"} component={CreditsScreen} options={{ ...screenOptions }} />
+            <RootStack.Screen name={"MediaPlayer"} component={MediaPlayer} options={{ ...screenOptions }} />
+          </RootStack.Group>
+          <RootStack.Group screenOptions={{ presentation: 'modal' }}>
+            <RootStack.Screen name={"Details"} component={DetailsScreen} options={{ headerShown: false }} />
+          </RootStack.Group>
+        </RootStack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
