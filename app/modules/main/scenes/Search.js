38f298e6c2c9f693de4525206f66630e2d6501de
Moses Esan <mosesesan@hotmail.com>
2024-12-17 03:25:35 +0000

    Added filter functionality to the Search scene and using the SearchBarContainer component

--- app/modules/main/scenes/Search.js
+++ app/modules/main/scenes/Search.js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect, useMemo, useEffect, useState } from 'react';
-import { SafeAreaView, ActivityIndicator, FlatList, StyleSheet, View, ScrollView, Platform, RefreshControl } from 'react-native';
+import { SafeAreaView, ActivityIndicator, FlatList, StyleSheet, View, ScrollView, Platform, RefreshControl, useWindowDimensions, Text } from 'react-native';
 
 // NAVIGATION
 import { useNavigation } from "@react-navigation/native";
@@ -8,7 +8,6 @@ import { useNavigation } from "@react-navigation/native";
 import axios from 'axios';
 import { EmptyView, ErrorView, CustomNavTitle } from "react-native-helper-views";
 import { PanelContainer } from "mesan-react-native-panel";
-import { SearchBar } from '@rneui/themed';
 
 // HOOKS
 import useFetch from '../hooks/useFetch';
@@ -18,6 +17,7 @@ import { getAllCategories, searchByName } from "../core/Service";
 
 // COMPONENTS
 import ModuleItem from "../components/ModuleItem";
+import SearchBarContainer from '../components/SearchBarContainer';
 
 // CONFIG
 import { colors } from "../core/Config";
@@ -29,14 +29,22 @@ export default function Search(props) {
 
     // LOADING STATE AND ERROR
     const [searchValue, setSearchValue] = useState("");
-    const [searchType, setSearchType] = useState(null);
     const [cancelToken, setCancelToken] = useState('');
     const [categories, setCategories] = useState([]);
 
-    const [       
+    const [
         { data, error, page, nextPage, totalResults, isFetching, isRefreshing, isFetchingNextPage },
         { setData, setError, setPage, setNextPage, setTotalResults, setIsFetching, setIsRefreshing, setLoadingState, setIsFetchingNextPage, setAPIResponse }
     ] = useFetch();
+
+    //FILTER OPTIONS
+    const FILTERS = [
+        { id: 1, name: "All", slug:"multi" },
+        { id: 2, name: "Movies", slug:"movie" },
+        { id: 3, name: "TV Show", slug:"tv"  },
+        { id: 4, name: "People", slug:"person"  }
+    ]
+    const [searchFilter, setSearchFilter] = useState(FILTERS[0]);
 
     //==================================================================================================
     //1B -NAVIGATION CONFIG
@@ -47,23 +55,27 @@ export default function Search(props) {
         });
     }, [navigation]);
 
-
     //==========================================================================================
     // 2 - MAIN CODE BEGINS HERE
     // ==========================================================================================
     // 2A - USE EFFECT - GET CATEGORIES
     useEffect(() => {
-        (async () => await _getCategories())();
+        (async () => await getCategories())();
     }, []);
 
     // 2A - USE EFFECT - SEARCH
     useEffect(() => {
         if (searchValue.length > 0) (async () => await search(searchValue, page))();
-        else setData([]);
-    }, [searchValue]);
+        else {
+            // Reset the page number and the selected filter and data
+            setPage(1);
+            setSearchFilter(FILTERS[0])
+            setData([]);
+        }
+    }, [searchValue, searchFilter]);
 
     // 2B - GET CAEGORIES
-    async function _getCategories(refresh=false, params = {}) {
+    async function getCategories(refresh = false, params = {}) {
         try {
             // set the loading state
             setLoadingState(!refresh, refresh);
@@ -73,7 +85,7 @@ export default function Search(props) {
                 // convert the key to title case
                 let title = key.replace(/\b\w/g, l => l.toUpperCase());
                 return ({ title: `${title} Categories`, data, type: "grid" })
-        });
+            });
             setCategories(panels || [])
         } catch (error) {
             setError(error.message);
@@ -86,7 +98,7 @@ export default function Search(props) {
     //2B - GET DATA
     async function search(query = searchValue, page = 1, more = false) {
         if (!more) setIsFetching(true);
-        
+
         // Cancel the previous request before making a new request
         if (cancelToken) cancelToken.cancel('Operation canceled by the user.');
 
@@ -95,10 +107,10 @@ export default function Search(props) {
         setCancelToken(cancelToken_);
 
         try {
-            let params = { page, query, cancelToken: cancelToken.token, "primary_release_date.lte": "31-12-2024" }
+            let params = { page, query, cancelToken: cancelToken.token }
 
             // Determine the slug to use
-            let paramSlug = searchType ? searchType.slug : 'multi';
+            let paramSlug = searchFilter ? searchFilter.slug : 'multi';
             let response = await searchByName(paramSlug, params);
             setAPIResponse(response, { page: "page", total_results: "total_results", total_pages: "total_pages", data: "results" });
         } catch (error) {
@@ -112,7 +124,7 @@ export default function Search(props) {
     // 2C - REFRESH DATA
     async function refetchData() {
         setIsRefreshing(true);
-        await _getCategories(true);
+        await getCategories(true);
     }
 
     // 2D - FETCH NEXT PAGE
@@ -123,27 +135,15 @@ export default function Search(props) {
         }
     }
 
-    // ==========================================================================================
-    //4 -  ACTION HANDLERS
-    //==========================================================================================
-    // If using Infinite Scroll - Load More Data
-    function onEndReached() {
-        if (!isFetching && !isFetchingNextPage && nextPage) {
-            fetchNextPage()
-        }
-    }
-
-    // ON CLOSE
-    const onClose = () => {
-        navigation.goBack();
-    }
-
     //==============================================================================================
-    //5 -  UI ACTION HANDLERS
+    //3 -  UI ACTION HANDLERS
     //==============================================================================================
     //3a - RENDER ITEM
     const renderItem = ({ item, index }) => {
-        let type = (searchValue.length === 0) ? 'category-grid' : 'media-grid';
+        let type = 'category-grid';
+        if (searchValue.length > 0){
+            type = searchFilter && searchFilter?.slug === "person" ? "person-grid" :  'media-grid';
+        }
         return <ModuleItem type={type} item={item} key={`search_item_${index}`} />
     }
 
@@ -163,13 +163,21 @@ export default function Search(props) {
 
     // 3d - RENDER ERROR
     const renderError = () => {
-        return <ErrorView message={error} onPress={search} />
+        const onPress = searchValue.length === 0 ? getCategories : search;
+        return <ErrorView message={error} onPress={onPress} titleStyle={{ color: "#fff" }} />
     }
 
     // 3e - RENDER LOADING
     const renderLoading = () => {
         return <ActivityIndicator style={{ backgroundColor: colors.primary, flex: 1 }} />
     }
+
+    const LazyPlaceholder = ({ route }) => (
+        <View style={styles.scene}>
+            <Text>Loading {route.title}…</Text>
+        </View>
+    );
+
 
     // 3f - RENDER VIEW
     const renderView = () => {
@@ -187,6 +195,29 @@ export default function Search(props) {
     }
 
     // ==========================================================================================
+    //4 -  ACTION HANDLERS
+    //==========================================================================================
+    // If using Infinite Scroll - Load More Data
+    function onEndReached() {
+        if (!isFetching && !isFetchingNextPage && nextPage) {
+            fetchNextPage()
+        }
+    }
+
+    // ON CLOSE
+    const onClose = () => {
+        navigation.goBack();
+    }
+
+    //ON FILTER SELECTED
+        // ON FILTER ITEM SELECTED
+        function onFilterSelected(selected) {
+            //RESET THE PAGE TO 1 AND SET THE SELETED FILTER
+            setPage(1);
+            setSearchFilter(selected);
+        };
+
+    // ==========================================================================================
     // 5 - VIEW PROPS
     //==========================================================================================
     //5a - FLATLIST PROPS
@@ -194,10 +225,6 @@ export default function Search(props) {
     const listData = useMemo(() => {
         if (searchValue.length === 0) return categories
         return data;
-        // return data && data.pages.map(page => {
-        //     if (page?.results) return page.data;
-        //     return []
-        // }).flat()
     }, [searchValue, data, categories])
 
     const listProps = {
@@ -207,7 +234,7 @@ export default function Search(props) {
         numColumns: 3,
 
         style: { backgroundColor: colors.primary },
-        contentContainerStyle: { flexGrow: 1, paddingHorizontal:10},
+        contentContainerStyle: { flexGrow: 1, paddingHorizontal: 10 },
 
         renderItem: renderItem,
         ListEmptyComponent: renderEmpty,
@@ -238,75 +265,31 @@ export default function Search(props) {
         horizontal: false,
         showsHorizontalScrollIndicator: false,
         refreshControl: refreshControl,
-        contentContainerStyle: { flexGrow: 1,  }
+        contentContainerStyle: { flexGrow: 1 }
     }
+
+    //5d - SEARCH PROPS
+    const searchBarProps = {
+        searchValue,
+        onChangeText: setSearchValue,
+        onClose: onClose,
+        placeholder: "Search by title",
+        filters: FILTERS,
+        filter:searchFilter,
+        onFilterSelected: onFilterSelected,
+        showFilter: searchValue.length > 0,
+        // onSortByPress: () => alert("sort")
+    };
 
     // ==========================================================================================
     // 6 - RENDER VIEW
     //==========================================================================================
     return (
         <SafeAreaView style={[{ flex: 1, backgroundColor: colors.primary }]}>
-            <SearchBarContainer searchValue={searchValue} onChangeText={setSearchValue} onClose={onClose} placeholder={"Search by title"} />
-            {(isFetching && !isFetchingNextPage) && renderLoading()}
-            {(error) && renderError()}
-            {renderView()}
+            <SearchBarContainer {...searchBarProps} />
+            {(isFetching && !isFetchingNextPage) ? renderLoading() : (error) ? renderError() : (
+                renderView()
+            )}
         </SafeAreaView>
     );
 };
-
-const SearchBarContainer = ({ searchValue, onChangeText, onClose, placeholder = "Type Here..." }) => {
-    return (
-        <View style={{ flexDirection: "row", paddingHorizontal: 4 }}>
-            <SearchBar
-                value={searchValue}
-                placeholder={placeholder}
-                onChangeText={onChangeText}
-                placeholderTextColor={"#fff"}
-                inputStyle={styles.inputStyle}
-                containerStyle={{flex: 1, backgroundColor: colors.primary, borderTopWidth:0, borderBottomWidth:0}}
-                inputContainerStyle={styles.inputContainerStyle}
-                showCancel={true}
-                autoFocus={false} />
-            {/* <Pressable onPress={onClose} style={styles.cancelButton}>
-                <Text style={styles.cancelText}>Cancel</Text>
-            </Pressable> */}
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#000"
-    },
-
-    containerStyle: {
-        flex: 1,
-        backgroundColor: "#000",
-        paddingVertical: 6,
-    },
-
-    inputContainerStyle: {
-        height: 44,
-        borderRadius: 12,
-        backgroundColor: "rgb(28,28,31)"
-    },
-
-    inputStyle: {
-        color: "#fff",
-        minHeight: 34,
-        fontSize: 16,
-    },
-
-    cancelButton: {
-        width: 60,
-        justifyContent: "center",
-        alignItems: "center",
-        marginRight: 8
-    },
-
-    cancelText: {
-        color: "#fff",
-        fontWeight: "6i00"
-    }
-});
