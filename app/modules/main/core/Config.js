8f6d764ab0cbd7c344084f997edb29517fcbef18
Moses Esan <mosesesan@hotmail.com>
2024-12-17 02:23:54 +0000

    Updated API URL TO v3, added Person endpoint and moved TMDB token here

--- app/modules/main/core/Config.js
+++ app/modules/main/core/Config.js
@@ -7,14 +7,18 @@ export const colors = {
 }
 
 // 2 - API URL AND ENDPOINTS
-export const CRUD_API_URL = "https://mesan-moflix-app-c00e8823b683.herokuapp.com/v2/"
+export const CRUD_API_URL = "https://mesan-moflix-app-c00e8823b683.herokuapp.com/v3/"
 export const SECTIONS_ENDPOINT = 'sections/'
 export const PANELS_ENDPOINT = 'panels/'
 
 export const TMDB_API_URL = 'https://api.themoviedb.org/3/'
 export const DISCOVER_ENDPOINT = 'discover/'
 export const SEARCH_ENDPOINT = 'search/'
+export const PERSON_ENDPOINT = 'person/'
 
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 export const LARGE_IMAGE_URL = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/";
 export const YOUTUBE_URL = "https://img.youtube.com/vi/";
+
+
+export const TMDB_API_TOKEN = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhM2UzYTFkNzkyMzVhMWVmMTg5ZGJiMGMyNDNmMjQwZSIsIm5iZiI6MTcyNjAxMTkzNS4zMTY3ODUsInN1YiI6IjU2MDJkMDc0YzNhMzY4NTU0MTAwMjE5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j0uqlo6JAwjS1dKZoakPNgls0R1g1kCA5gwmWhmH3mw`
