d9edf88b4b28c4d731874e80f501a3900b75bab6
Moses Esan <mosesesan@hotmail.com>
2024-12-17 02:21:21 +0000

    Provider file cleanup

--- app/modules/main/core/Provider.js
+++ app/modules/main/core/Provider.js
@@ -1,7 +1,6 @@
 import React, { useMemo, useContext, createContext, useState } from 'react';
 
 //CREATE REDUCER
-import { useCRUD } from "react-native-helper-views";
 
 // CONTEXT ==================================
 export const moduleContext = createContext();
@@ -14,41 +13,14 @@ export default function ModuleProvider(props) {
     const [sections, setSections] = useState([])
     const [section, setSection] = useState({})
 
-    // CRUD HOOK
-    const dataProvider = useCRUD([])
-
     // ==========================================================================================
     // 2 - ACTION HANDLERS
-    const setData = (data) => {
-        dataProvider.setData(data)
-        dataProvider.setError(null)
-        reset();
-    };
 
-    const setError = (error) => {
-        dataProvider.setError(error)
-        reset();
-    };
-
-    const reset = () => {   
-        dataProvider.setIsFetching(false)
-        dataProvider.setIsRefreshing(false)
-    }
-
-    const clearData = () => {
-        dataProvider.setData([])
-    };
-
-    const memoizedValues = { data, error, isFetching, isRefreshing, setLoading} = dataProvider;
     const value = useMemo(() => ({
-        ...memoizedValues,
-        dataProvider,
-        setData, clearData, setError,
-
         sections, setSections,
         section, setSection
 
-    }), [data, error, isFetching, isRefreshing, section]);
+    }), [section]);
 
     return (
         <Provider value={value}>
